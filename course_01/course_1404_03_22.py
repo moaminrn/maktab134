c60092a3e977cf2712e1bfe1872c912c2e630e57
amir-h chegouniyan <amirch7294@gmail.com>
2025-07-11 14:31:46 +0330

    variables, conditions, expresions

---
+++ course_01/course_1404_03_22.py
@@ -0,0 +1,124 @@
+phone_number = 125
+phone_number = 125
+# int ddddd = 123
+
+# phone_number = "ddddd"
+# a-z A-Z 0-9 _
+
+# snake case -> phone number, phonenumber,phone_number -> OK
+# camel case -> phoneNumber
+
+# 123phone_number = 125
+# char(a-z A-Z) _
+
+phone_number123 = 123
+_phone_number = 124
+
+# print(phone_number)
+# print("Welcome")
+
+# print(125)
+# print(123.5)
+# print("phone_number")
+# print(phone_number, type(phone_number))
+# phone_number = "123"
+# print(phone_number, type(phone_number))
+
+
+# Type -> int(1,2,40,1390, -3) | float(1.0, 1.5, 10.3, 0.5)
+# str -> "ddsf2323", '333', "|%", "$@"
+# 333 != '333'
+
+# print(type(True))
+# bool -> True | False -> 1 | 0
+gender = True
+gender = False
+is_authenticated = False
+
+user_gender = True
+is_authenticated = True
+
+
+# age = 44
+# name = "amir"
+
+
+# age = int(input("Please input your age: "))
+# age = input("Please input your age: ")
+name = input("Please input your name: ")
+
+
+
+
+# str(), int(), float(), bool()
+
+# print("Your age is: ", age)
+# print("Your name is: ", name)
+
+# print(type(age), type(name))
+# print("Your age is: ", age, ".\nYour name is: ", name)
+# \n -> new line
+# \t -> tab
+
+# print(f"Your age is: {age}.\nYour name is: {name}.")
+
+# conditions
+# if
+# if <expression>:
+#   print()
+
+# expression
+# bool -> True | False
+# var|value oper var|value
+# 12     12
+# == <= >= != < >
+
+# if name == "amir" or name == "ali":
+#     print(name == "amir")
+#     print(name == "ali")
+#     print(name == "amir" or name == "ali")
+#     print("Welcome")
+
+# # if name == "amir":
+# #     print("Welcome")
+
+# # if name == "ali":
+# #     print("Welcome")
+
+# if name != "amir":
+#     print("Hello")
+
+# if else
+
+# if name == "amir" or name == "ali":
+#     print("Welcome")
+
+# else:
+#     if name == "maryam":
+#         print("Hello")
+#     else:
+#         print("Hi")
+
+# elif
+
+# if name == "amir" or name == "ali":
+#     print("Welcome")
+
+# elif name != "maryam":
+#     print("Hello")
+
+# else:
+#     print("Hi")
+
+# if not age.isnumeric():
+#     print("Your age is not a number")
+# else:
+#     if int(age) == 40:
+#         print("Welcome")
+
+# isnumeric
+
+# + - / * ** % //
+
+print(234%5)
+print(4**4)
