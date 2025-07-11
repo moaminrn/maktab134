e7e8c2b9218398558f3928c583d4402d7fb0f4f5
amir-h chegouniyan <amirch7294@gmail.com>
2025-07-11 15:21:52 +0330

    f-string, list, loop

---
+++ course_02/course_1404_03_23.py
@@ -0,0 +1,120 @@
+a = 12
+b = "amir"
+c = 40.3
+
+# print(f"Hello {b}")
+
+a_list = [12, "amir", 40.3]
+class_score = [12, 20, 15, 16.5, 8]
+
+
+class_student_list = ["Aryan", "Parniyan", "Sepide", "Atefe", "Mohammad amin"]
+
+# print(f"Hello {class_student_list[0]}")
+# print(f"Hello {class_student_list[1]}")
+# print(f"Hello {class_student_list[2]}")
+# print(f"Hello {class_student_list[3]}")
+# print(f"Hello {class_student_list[4]}")
+# print(f"Hello {class_student_list[4]}")
+# print(f"Hello {class_student_list[5]}")
+# print(f"Hello {class_student_list[6]}")
+# print(f"Hello {class_student_list[1]}")
+
+
+# print(f"Hello {class_student_list}")
+
+# name_of_var =  [............,....,.....]
+
+# loop
+# for
+
+# for student in class_student_list:
+#    print(f"Hello {student}")
+
+
+score = [100, 50, 203, 900, 12, 305, 42, 52, 887, 990]
+
+min_number = score[0]
+max_number = score[0]
+sum_of_list = 0
+count_of_list = 0
+
+# for number in score:
+#     # sum_of_list = sum_of_list + number
+#     sum_of_list += number
+#     # count_of_list = count_of_list + 1
+#     count_of_list += 1
+#     if number < min_number:
+#         min_number = number
+
+#     if number > max_number:
+#         max_number = number
+
+# avg = sum_of_list/count_of_list
+# print(f"Average of list is: {avg}")
+# print(f"Min number of list is: {min_number}")
+# print(f"Max number of list is: {max_number}")
+
+
+# List
+# name = [6565, 53, 23, "sd"]
+
+# print(len(name))
+print(score)
+# print(len(score))
+score.append([23, 45, 67])
+print(score)
+# new_array = score[10]
+# print(new_array)
+# print(score[10][1])
+# print(score[10][1])
+# print(score)
+# print(len(score))
+
+# score.insert(5, 9999)
+# print(score)
+
+# score.pop(3)
+# print(score)
+
+# score.pop()
+# print(score)
+# print(max(score))
+# print(min(score))
+
+
+users_info = [
+    ["ali", "mohammadi", 18],
+    ["ariyan", "ali", 24],
+    ["atefe", "farahbod", 30],
+]
+
+# for user in users_info:
+#     print(f"{user[0]} {user[1]} {user[2]}")
+
+
+# print(users_info[0:2])
+# # users_info.reverse()
+# print(users_info)
+
+# # print(users_info)
+
+# print(score)
+score.append([23, 45, 67])
+# print(score)
+# print(len(score))
+score.pop()
+# print(score.pop())
+# # {} [] ()
+# score.extend([23, 45, 67])
+# print(score)
+# print(len(score))
+
+
+print(score.reverse())
+
+# score.extend(score[0:3])
+# print(score)
+
+
+print(type(None))
