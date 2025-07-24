81c4b4cf8a4e8fdd299370ecae47369b53fc70c0
amir-h chegouniyan <amirch7294@gmail.com>
2025-07-24 13:09:52 +0330

    abstraction and abstract method

---
+++ course_11/course_1404_05_02.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class Car(ABC):
+    @abstractmethod
+    def change_tire(self):
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Benz(Car):
+    def change_tire(self):
+        print("Change tire")
+
+    def start(self):
+        print("Start")
+
+
+# c1 = Car()
+b1 = Benz()
+b1.change_tire()
+b1.start()
